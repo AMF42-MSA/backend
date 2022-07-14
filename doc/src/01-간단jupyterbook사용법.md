3ea1885d2f9bd7f8d495f7e2dfc71a3fcb978cab
myinno <jcscom13@gmaiol.com>
2022-07-14 17:46:07 +0900

    :art: aa

--- doc/src/01-간단jupyterbook사용법.md
+++ doc/src/01-간단jupyterbook사용법.md
@@ -45,6 +45,10 @@ title "jupyter book 만들기"
 # 생성위치는 각자 수정(d:\APP\GIT-AMF3\backend\doc\htm)
 # 해당 shell 수행위치는 build.msa가 있는 위치에서 수행
 jb build  --path-output "d:\APP\GIT-AMF3\backend\doc\html"  .\
+
+# 만약 목차가 많이 수정되거나, "다음", "이전"이 잘못 연결되면 "--all" option 추가하여 빌드
+#jb build  --all --path-output "d:\APP\GIT-AMF3\backend\doc\html"  .\
+
 ```
 
 
--- doc/src/build.cmd
+++ doc/src/build.cmd
@@ -3,6 +3,9 @@ chcp 65001
 
 title "jupyter book 만들기"
 
-#set HTML_ROOT_DIR=c:\APP\jb_html
-
+rem 생성위치는 각자 수정(d:\APP\GIT-AMF3\backend\doc\htm)
+rem 해당 shell 수행위치는 build.msa가 있는 위치에서 수행
 jb build  --path-output "d:\APP\GIT-AMF3\backend\doc\html"  .\
+
+rem 만약 목차가 많이 수정되거나, "다음", "이전"이 잘못 연결되면 "--all" option 추가하여 빌드
+rem jb build  --all --path-output "d:\APP\GIT-AMF3\backend\doc\html"  .\
