97eed33f6af21c87e665f96e0fbc74a0c7ac0e85
EunmiKang <hellokem26@gmail.com>
2022-07-29 22:04:30 +0900

    [Lecture-Category] CrudRepository -> JPARepository로 변경

--- lecturecategory-domain/src/main/java/com/everyoneslecture/lecturecategory/domain/LectureCategoryRepository.java
+++ lecturecategory-domain/src/main/java/com/everyoneslecture/lecturecategory/domain/LectureCategoryRepository.java
@@ -2,8 +2,8 @@ package com.everyoneslecture.lecturecategory.domain;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LectureCategoryRepository extends CrudRepository<LectureCategory, Long>{
+public interface LectureCategoryRepository extends JpaRepository<LectureCategory, Long>{
   Optional<LectureCategory> findByCategoryName(String categoryName);
 }
