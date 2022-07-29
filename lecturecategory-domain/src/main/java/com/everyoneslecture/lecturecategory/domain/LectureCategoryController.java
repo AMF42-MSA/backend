1b421388ef0df9724f3cc8526f1665864d2771c6
EunmiKang <hellokem26@gmail.com>
2022-07-30 01:05:21 +0900

    [Lecture-Category] registerLectureCategory GET/POST용 함수 따로 구현

--- lecturecategory-domain/src/main/java/com/everyoneslecture/lecturecategory/domain/LectureCategoryController.java
+++ lecturecategory-domain/src/main/java/com/everyoneslecture/lecturecategory/domain/LectureCategoryController.java
@@ -1,10 +1,16 @@
 package com.everyoneslecture.lecturecategory.domain;
 
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +21,31 @@ public class LectureCategoryController {
   @Autowired
   LectureCategoryRepository lectureCategoryRepository;
 
-  @RequestMapping(value="lectureCategories/registerCategory")
-  public Long registerLectureCategory(@RequestParam String categoryName) {
+  @RequestMapping(value="lectureCategories/registerCategory", method=RequestMethod.GET)
+  public Long registerLectureCategoryGet(@RequestParam String categoryName) {
     Long result = Long.valueOf(-1);
 
-    System.out.println("categoryName: " + categoryName);
+    //System.out.println("************GET******************** categoryName: " + categoryName);
+    Optional<LectureCategory> lectureCategory = lectureCategoryRepository.findByCategoryName(categoryName);
+    if(lectureCategory.isPresent()) {
+      // 이미 해당 카테고리명 존재할 경우, return -1
+      return result;
+    }
+
+    // 카테고리 등록
+    LectureCategory newLectureCategory = new LectureCategory();
+    newLectureCategory.setCategoryName(categoryName);
+    result = lectureCategoryRepository.save(newLectureCategory).getCategoryId();
+
+    return result;
+  }
+
+  @RequestMapping(value="lectureCategories/registerCategory", method=RequestMethod.POST)
+  public Long registerLectureCategoryPOST(@RequestBody Map<String, String> paramMap) {
+    Long result = Long.valueOf(-1);
+
+    String categoryName = paramMap.get("categoryName");
+    //System.out.println("************POST******************** categoryName: " + categoryName);
     Optional<LectureCategory> lectureCategory = lectureCategoryRepository.findByCategoryName(categoryName);
     if(lectureCategory.isPresent()) {
       // 이미 해당 카테고리명 존재할 경우, return -1
