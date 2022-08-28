c96310986eb181de8b7dca4e69d29337bbeedae8
hellokem26 <hellokem26@gmail.com>
2022-08-28 23:28:54 +0900

    [Lecture-Category] 강의분류 수정,삭제 이벤트 추가

--- lecturecategory-domain/src/main/java/everyoneslecture/lecturecategory/domain/lecturecategory/entity/LectureCategory.java
+++ lecturecategory-domain/src/main/java/everyoneslecture/lecturecategory/domain/lecturecategory/entity/LectureCategory.java
@@ -3,6 +3,8 @@ package everyoneslecture.lecturecategory.domain.lecturecategory.entity;
 import javax.persistence.*;
 
 import everyoneslecture.lecturecategory.domain.lecturecategory.event.LectureCategoryRegistered;
+import everyoneslecture.lecturecategory.domain.lecturecategory.event.LectureCategoryModified;
+import everyoneslecture.lecturecategory.domain.lecturecategory.event.LectureCategoryDeleted;
 
 @Entity
 public class LectureCategory {
@@ -33,6 +35,26 @@ public class LectureCategory {
         categoryRegistered.setCategoryName(this.getCategoryName());
 
         categoryRegistered.publishAfterCommit();
+    }
+
+    @PostUpdate
+    public void postUpdate() {
+        LectureCategoryModified categoryModified = new LectureCategoryModified();
+
+        categoryModified.setCategoryId(this.getCategoryId());
+        categoryModified.setCategoryName(this.getCategoryName());
+
+        categoryModified.publishAfterCommit();
+    }
+
+    @PostRemove
+    public void postRemove() {
+        LectureCategoryDeleted categoryDeleted = new LectureCategoryDeleted();
+
+        categoryDeleted.setCategoryId(this.getCategoryId());
+        categoryDeleted.setCategoryName(this.getCategoryName());
+
+        categoryDeleted.publishAfterCommit();
     }
 
 
