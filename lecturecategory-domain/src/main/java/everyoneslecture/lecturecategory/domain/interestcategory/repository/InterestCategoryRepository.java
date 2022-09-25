4ca7c6281dd0ae73f7922cd53e49290ff736236a
hellokem26 <hellokem26@gmail.com>
2022-09-25 21:06:35 +0900

    [LectureCategory] top5관심분류 쿼리 MySQL용으로 변경..

--- lecturecategory-domain/src/main/java/everyoneslecture/lecturecategory/domain/interestcategory/repository/InterestCategoryRepository.java
+++ lecturecategory-domain/src/main/java/everyoneslecture/lecturecategory/domain/interestcategory/repository/InterestCategoryRepository.java
@@ -20,16 +20,18 @@ public interface InterestCategoryRepository extends JpaRepository<InterestCatego
   @Query(value = "SELECT * FROM interest_category WHERE email = :email AND category_id = :categoryId", nativeQuery = true)
   Optional<InterestCategory> findByMemberEmailAndCategoryId(@Param("email") String email, @Param("categoryId") Long categoryId);
 
-  @Query(value = "SELECT TOP(5) category_id as categoryId, category_name as categoryName, count(0) as categoryCnt \n" +
+  @Query(value = "SELECT category_id as categoryId, category_name as categoryName, count(0) as categoryCnt \n" +
                   "FROM interest_category \n" +
-                  "WHERE REGISTRATION_DATE >= (sysdate-7) \n" +
-                  "GROUP BY category_id \n" +
-                  "ORDER BY categoryCnt DESC, category_id DESC", nativeQuery = true)
+                  "WHERE REGISTRATION_DATE >= (sysdate()-7) \n" +
+                  "GROUP BY category_id, category_name \n" +
+                  "ORDER BY categoryCnt DESC, category_id DESC \n" +
+                  "LIMIT 5", nativeQuery = true)
   List<TopInterestCategoryDTO> recentTop5InterestCategory();
 
-  @Query(value = "SELECT TOP(5) category_id as categoryId, category_name as categoryName, count(0) as categoryCnt \n" +
+  @Query(value = "SELECT category_id as categoryId, category_name as categoryName, count(0) as categoryCnt \n" +
                   "FROM interest_category \n" +
-                  "GROUP BY category_id \n" +
-                  "ORDER BY categoryCnt DESC, category_id DESC", nativeQuery = true)
+                  "GROUP BY category_id, category_name \n" +
+                  "ORDER BY categoryCnt DESC, category_id DESC \n" +
+                  "LIMIT 5", nativeQuery = true)
   List<TopInterestCategoryDTO> top5InterestCategory();
 }
