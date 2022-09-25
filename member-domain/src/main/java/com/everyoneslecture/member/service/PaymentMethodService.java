603d08a615fa6ce9ab033eca27c22fcec9805574
Joon <simair7@gmail.com>
2022-09-25 10:37:00 +0900

    [0925] PaymentMethod 수정

--- member-domain/src/main/java/com/everyoneslecture/member/service/PaymentMethodService.java
+++ member-domain/src/main/java/com/everyoneslecture/member/service/PaymentMethodService.java
@@ -1,6 +1,7 @@
 package com.everyoneslecture.member.service;
 
 import com.everyoneslecture.member.domain.paymentMethod.dto.PaymentMethodDto;
+import com.everyoneslecture.member.domain.paymentMethod.dto.RequestPaymentMethod;
 
 public interface PaymentMethodService {
     PaymentMethodDto registerPaymenetMethod(PaymentMethodDto paymentMethodDto);
